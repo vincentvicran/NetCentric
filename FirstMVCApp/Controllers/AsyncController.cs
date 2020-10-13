7fdf61c3bf26f3e0c9601d5a05025517f1bedbeb
vincentvicran <vvicran@gmail.com>
2020-10-13 09:40:57 +0545

    update upto 13.10.2020

---
+++ FirstMVCApp/Controllers/AsyncController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class AsyncController : Controller
+    {
+
+        List<string> NameList = new List<string>();
+        int counter = 0;
+        public IActionResult Index()
+        {
+            FirstFunctionAsync();
+            SecondFunction();
+            return View();
+        }
+
+        public async Task FirstFunctionAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (; counter <= 100; counter++)
+                {
+                    NameList.Add("first function executed");
+                }
+            }
+            );
+           
+        }
+
+        public void SecondFunction()
+        {
+            
+                for (; counter <= 100; counter++)
+                {
+                    NameList.Add("first function executed");
+                }
+            
+        }
+
+      /*public async void SecondFunction()
+        {
+            await Task.Run(() =>
+            {
+                for (; counter <= 100; counter++)
+                {
+                    NameList.Add("first function executed");
+                }
+            });
+        }*/
+    }
+}
